aadf712a2c21881e26bd682accfe02f4c1adbc06
Victor <victorokolie001@gmail.com>
2025-02-09 19:40:50 +0100

    finished dessigns

---
+++ main.js
@@ -0,0 +1,12 @@
+const Quiz = [
+    {
+       question: 'Which of these players holds the record for the most three point made in the NBA',
+       options: ['Ray Allen', 'Stephen Curry', 'James Harden', 'Reggie Miller']
+    }
+]
+
+
+
+function loadQuestions () {
+
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+load questions
+view current Question
+display options of all active question
+select an answer
+track your score sheet
+view your result
